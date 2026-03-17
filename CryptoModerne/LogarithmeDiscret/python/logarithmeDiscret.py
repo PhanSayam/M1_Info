e55c8c4032bcf8d88880596cf1e01da38d1cde3f
Damien Phan <damien-phan@etud.univ-tln.fr>
2026-03-17 13:17:54 +0100

    log_discret python

---
+++ CryptoModerne/LogarithmeDiscret/python/logarithmeDiscret.py
@@ -0,0 +1,39 @@
+import math
+
+dico = {}
+
+# p=1934459 
+# g=762973 
+# b=1191663
+
+p=6127307 
+g=4268803 
+b=2108349
+
+# Phase 1
+# m = partie entiere de la racine carrée de p
+m = int(math.sqrt(p))
+
+val = 1
+dico[val] = 0
+for i in range(1,m):
+    val = (val * g) % p
+    dico[val] = i
+    
+# Phase 2
+j = 0
+g_m = pow(g, -m, p)
+z = b 
+while j <= m :    
+    if z in dico : 
+        x = m*j + dico[z]
+        break 
+    z = (z * g_m) % p
+    j = j +1   
+
+
+g_x = pow(g,x,p)
+print("g^x = ", g_x)
+print("b   = ", b)
+
+
